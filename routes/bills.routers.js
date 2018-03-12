e1290ed1ae0d306b4ed6e3e53eec08fbab0478bf
Adrian <adriantoddross@gmail.com>
2018-03-12 17:17:37 -0400

    change array to include strings and not objects

--- routes/bills.routers.js
+++ routes/bills.routers.js
@@ -6,18 +6,13 @@ const router = express.Router();
 router.get('/bills', (req, res, next) => {
 
   const bills = [
-    {
-      name: 'Internet',
-      amount: 65
-    },
-    {
-      name: 'Groceries',
-      amount: 150
-    },
-    {
-      name: 'Utilities',
-      amount: 250
-    }
+    'Internet $65',
+    'Groceries $250',
+    'Utilities $300',
+    'Rent $1200',
+    'Cellphone $50',
+    'Netflix $15',
+    'Spotify $15'
   ];
 
   return res.json(bills);
