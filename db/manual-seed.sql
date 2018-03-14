74211f2b9a166c16e9f722727fdaf26ed437d982
Adrian <adriantoddross@gmail.com>
2018-03-14 05:18:08 -0400

    add sample data

--- db/manual-seed.sql
+++ db/manual-seed.sql
@@ -1,2 +1,8 @@
--- INSERT into categories (title) VALUES
--- ('Housing'), ('Childcare'), ('Food'), ('Healthcare'), ('Utilities'),('Auto'), ('Auto-pay'),
+INSERT into categories (title) VALUES
+('Housing'), ('Childcare'), ('Food'), ('Healthcare'), ('Utilities'),('Auto'), ('Auto-pay'), ('Entertainment')
+
+INSERT into users (fullname, username, password) VALUES
+('dev user', 'dev', '0320')
+
+INSERT into users (category_id, user_id, name, amount) VALUES
+(100, 1, 'Internet', '65'), (102, 1, 'Groceries', '250'), , (105, 1, 'Utilities', '350'), (100, 1, 'Rent', '1250'), (106, 1, 'Cellphone', '300'), (108, 1, 'Netflix', '15'), (108, 1, 'Spotify', '15')
