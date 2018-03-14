2c4d8e1bfa0a7bd2242846d89fdc45909b29c2f4
Adrian <adriantoddross@gmail.com>
2018-03-14 15:03:00 -0400

    add knex insert/query for POST method

--- routes/bills.routers.js
+++ routes/bills.routers.js
@@ -17,4 +17,47 @@ router.get('/bills', (req, res, next) => {
     });
 });
 
+router.post('/bills', (req, res, next) => {
+  const { name, amount, category_id } = req.body;
+
+  if (!req.body.name || !req.body.amount) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  const newBill = {
+    name: name,
+    amount: amount,
+    category_id: category_id
+  };
+
+  let billId;
+
+  const knex = dbGet();
+  knex.insert(newBill)
+    .into('bills')
+    .returning('id')
+    .then(([id]) => {
+      billId = id;
+    })
+    .then(() => {
+      return knex.select('bills.id', 'bills.category_id', 'bills.user_id', 'name', 'amount')
+        .from('bills')
+        .leftJoin('categories', 'bills.category_id', 'categories.id')
+        .leftJoin('bills_categories', 'bills.id', 'bills_categories.bill_id')
+        .where('bills.id', billId);
+    })
+    .then(result => {
+      if (result) {
+        res.location(`${req.originalUrl}/${billId}`).status(201).json;
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
 module.exports = router;
