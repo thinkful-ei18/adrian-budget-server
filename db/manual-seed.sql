b43e13214e066e2663c504c120ea58fd1ba41ea4
Adrian <adriantoddross@gmail.com>
2018-03-14 05:32:56 -0400

    add bills_categories

--- db/manual-seed.sql
+++ db/manual-seed.sql
@@ -1,8 +1,11 @@
 INSERT into categories (title) VALUES
-('Housing'), ('Childcare'), ('Food'), ('Healthcare'), ('Utilities'),('Auto'), ('Auto-pay'), ('Entertainment')
+('Housing'), ('Childcare'), ('Food'), ('Healthcare'), ('Utilities'),('Auto'), ('Auto-pay'), ('Entertainment');
 
-INSERT into users (fullname, username, password) VALUES
-('dev user', 'dev', '0320')
+INSERT into users (id, fullname, username, password) VALUES
+(1, 'dev user', 'dev', '0320');
 
-INSERT into users (category_id, user_id, name, amount) VALUES
-(100, 1, 'Internet', '65'), (102, 1, 'Groceries', '250'), , (105, 1, 'Utilities', '350'), (100, 1, 'Rent', '1250'), (106, 1, 'Cellphone', '300'), (108, 1, 'Netflix', '15'), (108, 1, 'Spotify', '15')
+INSERT into bills (category_id, user_id, name, amount) VALUES
+(100, 1, 'Internet', '65'), (102, 1, 'Groceries', '250'), (105, 1, 'Utilities', '350'), (100, 1, 'Rent', '1250'), (106, 1, 'Cellphone', '300'), (107, 1, 'Netflix', '15'), (107, 1, 'Spotify', '15');
+
+INSERT into bills_categories (bill_id, category_id) VALUES
+(1000, 100), (1001, 102), (1002, 105), (1003, 100), (1004, 106), (1005, 107), (1006, 107);
