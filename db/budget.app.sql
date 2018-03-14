29c2dcaf83cb0fec8230570f703d37264f7314ad
Adrian <adriantoddross@gmail.com>
2018-03-14 04:25:12 -0400

    alter sequence # for categories and bills

--- db/budget.app.sql
+++ db/budget.app.sql
@@ -5,9 +5,10 @@ CREATE TABLE categories (
   title text NOT NULL UNIQUE
 );
 
+ALTER SEQUENCE categories_id_seq RESTART WITH 100;
+
 CREATE TABLE users (
   id serial PRIMARY KEY,
-  -- bill_id int REFERENCES bills ON DELETE SET NULL,
   fullname text NOT NULL,
   username text NOT NULL UNIQUE,
   password text NOT NULL,
@@ -18,10 +19,12 @@ CREATE TABLE bills (
   id serial PRIMARY KEY,
   category_id int REFERENCES categories ON DELETE RESTRICT,
   user_id int REFERENCES users ON DELETE SET NULL,
-  title text NOT NULL,
+  name text NOT NULL,
   amount number NOT NULL,
   created timestamp DEFAULT now()
 );
+
+ALTER SEQUENCE bills_id_seq RESTART WITH 1000;
 
 CREATE TABLE bills_categories (
   bill_id INTEGER NOT NULL REFERENCES bills ON DELETE CASCADE,
