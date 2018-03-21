e7e1e6c7e8a88fb35d570145794245c3c52c84c4
Adrian <adriantoddross@gmail.com>
2018-03-21 15:38:56 -0400

    import express/express router and dbGet

--- routes/users.router.js
+++ routes/users.router.js
@@ -3,4 +3,10 @@
 const express = require('express');
 const router = express.Router();
 
+const {dbGet} = require('..db/knex');
+
+router.post ('/users', (req, res, next) => {
+  const knex = dbGet();
+});
+
 module.exports = router;
