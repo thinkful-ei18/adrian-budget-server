7f53e408a90139c8a25f8958ac56ae49f92c27f4
Adrian <adriantoddross@gmail.com>
2018-03-21 16:03:19 -0400

    add post method

--- routes/users.router.js
+++ routes/users.router.js
@@ -7,6 +7,38 @@ const {dbGet} = require('..db/knex');
 
 router.post ('/users', (req, res, next) => {
   const knex = dbGet();
+
+  const {fullname, username, password} = req.body;
+
+  const newUser = {
+    fullname: fullname,
+    username: username,
+    password: password
+  };
+
+  let userId;
+
+  knex.insert(newUser)
+    .into('users')
+    .returning('id')
+    .then(([id]) => {
+      userId = id;
+    })
+    .then(() => {
+      return knex.select('fullname', 'username')
+        .where('users.id', userId)
+        .first();
+    })
+    .then (result => {
+      if (result) {
+        res.location();
+      } else {
+        next();
+      }
+    })
+    .catch (err => {
+      next(err);
+    })
 });
 
 module.exports = router;
