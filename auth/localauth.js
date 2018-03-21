66310987d15ae5dcd35ede094b6408da396c2ca6
Adrian <adriantoddross@gmail.com>
2018-03-21 17:35:59 -0400

    starting over...

--- auth/localauth.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const express = require('express');
-const router = express.Router();
-
-const passport = require('passport');
-
-const options = {session: false, failwithError: true};
-const localAuth = passport.authenticate('local', options);
---
+++ passport/local.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const express = require('express');
+const passport = require('passport');
+const { Strategy: LocalStrategy } = require('passport-local');
+
+const app = express();
+app.use(express.static('public'));
+app.use(express.json());
