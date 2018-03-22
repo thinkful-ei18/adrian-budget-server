c7a5a464dbd8ff0e8c512060545c80d360e4270f
Adrian <adriantoddross@gmail.com>
2018-03-21 23:00:43 -0400

    import passport

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,6 +1,15 @@
 'use strict';
 
 const express = require('express');
+const passport = require(passport);
 const router = express.Router();
 
-const {dbGet} = require('../db-knex');
+// const {dbGet} = require('../db-knex');
+
+const options = {session: false, failWithError: true};
+
+const localAuth = passport.authenticate('local', options);
+
+router.post('/login', localAuth, function (req, res) {
+  return res.json(req.user);
+});
