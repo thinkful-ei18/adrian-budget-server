5ab4e7f7780fa5f5dc9c787c5374bc124d2b17b1
Adrian <adriantoddross@gmail.com>
2018-03-22 17:01:19 -0400

    import jwt, secret, expiry and return jwt on login

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -3,13 +3,23 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const {JWT_SECRET, JWT_EXPIRY} = require('../config');
 
 const options = {session: false, failWithError: true};
 
 const localAuth = passport.authenticate('local', options);
 
+function createAuthToken (user) {
+  return jwt.sign({ user }, JWT_SECRET, {
+    subject: user.username,
+    expiresIn: JWT_EXPIRY
+  });
+}
+
 router.post('/login', localAuth, function (req, res) {
-  return res.json(req.user);
+  const authToken = createAuthToken(req.user);
+  return res.json({ authToken });
 });
 
 module.exports = router;
