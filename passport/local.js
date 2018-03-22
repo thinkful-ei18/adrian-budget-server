9165823b43d3788637559ea40cad57bbe47bc8dc
Adrian <adriantoddross@gmail.com>
2018-03-21 22:28:24 -0400

    finish localStrategy

--- passport/local.js
+++ passport/local.js
@@ -7,3 +7,46 @@ const { Strategy: LocalStrategy } = require('passport-local');
 const app = express();
 app.use(express.static('public'));
 app.use(express.json());
+
+const {dbGet} = require('../db-knex');
+
+const localStrategy = new LocalStrategy((username, password, done) => {
+  const knex = dbGet();
+
+  // let userId = req.params.id;
+  let user;
+
+  knex.select('users.id', 'users.username', 'users.password')
+    .from('users')
+    .where('users.username', username)
+    .first()
+    .then(results => {
+      user = results;
+      if (!user) {
+        return Promise.reject({
+          reason: 'LoginError',
+          message: 'Incorrect username',
+          location: 'username'
+        });
+      }
+      return user.validatePassword(password);
+    })
+    .then(isValid => {
+      if (!isValid) {
+        return Promise.reject({
+          reason: 'LoginError',
+          message: 'Incorrect password',
+          location: 'password'
+        });
+      }
+      return done(null, user);
+    })
+    .catch(err => {
+      if(err.reason === 'LoginError') {
+        return done(null, false);
+      }
+      return done(err);
+    });
+});
+
+module.exports = localStrategy;
