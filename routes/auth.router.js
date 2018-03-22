5cc1b7ff9c9b57444be2dee4a34646ca8994356f
Adrian <adriantoddross@gmail.com>
2018-03-21 23:11:44 -0400

    remove dbGet import

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -4,8 +4,6 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 
-// const {dbGet} = require('../db-knex');
-
 const options = {session: false, failWithError: true};
 
 const localAuth = passport.authenticate('local', options);
