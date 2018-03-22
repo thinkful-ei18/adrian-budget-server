cc3739befa5692bfb386cd38aa9e084b4dbd1090
Adrian <adriantoddross@gmail.com>
2018-03-22 01:16:35 -0400

    test basic local authentication

--- passport/local.js
+++ passport/local.js
@@ -7,7 +7,6 @@ const { Strategy: LocalStrategy } = require('passport-local');
 const app = express();
 const bodyParser = require('body-parser');
 
-
 const {dbGet} = require('../db-knex');
 
 app.use(express.static('public'));
@@ -18,37 +17,56 @@ const localStrategy = new LocalStrategy((username, password, done) => {
   const knex = dbGet();
   let user;
 
-  knex.select('users.username', 'users.password')
-    .from('users')
-    .where('users.username', username)
-    .first()
-    .then(results => {
-      user = results;
-      if (!user) {
-        return Promise.reject({
-          reason: 'LoginError',
-          message: 'Incorrect username',
-          location: 'username'
-        });
-      }
-      return user.validatePassword(password);
-    })
-    .then(isValid => {
-      if (!isValid) {
-        return Promise.reject({
-          reason: 'LoginError',
-          message: 'Incorrect password',
-          location: 'password'
-        });
-      }
-      return done(null, user);
-    })
-    .catch(err => {
-      if(err.reason === 'LoginError') {
-        return done(null, false);
-      }
-      return done(err);
-    });
+  // knex.select('users.username', 'users.password')
+  //   .from('users')
+  //   .where('users.username', username)
+  //   .first()
+  //   .then(results => {
+  //     user = results;
+  //     if (!user) {
+  //       return Promise.reject({
+  //         reason: 'LoginError',
+  //         message: 'Incorrect username',
+  //         location: 'username'
+  //       });
+  //     }
+  //     return user.validatePassword(password);
+  //   })
+  //   .then(isValid => {
+  //     if (!isValid) {
+  //       return Promise.reject({
+  //         reason: 'LoginError',
+  //         message: 'Incorrect password',
+  //         location: 'password'
+  //       });
+  //     }
+  //     return done(null, user);
+  //   })
+  //   .catch(err => {
+  //     if(err.reason === 'LoginError') {
+  //       return done(null, false);
+  //     }
+  //     return done(err);
+  //   });
+
+  try {
+    if (username !== 'thinkfulstudent123') {
+      console.log('Incorrect username!');
+      return done(null, false);
+    }
+
+    if (password !== 'thinkful2018') {
+      console.log('Incorrect password!');
+      return done(null, false);
+    }
+
+    const user = { username, password };
+    return done(null, user);
+
+  } catch (err) {
+    done(err);
+  }
+
 });
 
 module.exports = localStrategy;
