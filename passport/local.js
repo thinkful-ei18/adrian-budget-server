8882fc8b476d4be90e63069cc1c7086c22e8a463
Adrian <adriantoddross@gmail.com>
2018-03-22 02:04:32 -0400

    console.log password

--- passport/local.js
+++ passport/local.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const passport = require('passport');
 const { Strategy: LocalStrategy } = require('passport-local');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 const bodyParser = require('body-parser');
@@ -17,55 +18,60 @@ const localStrategy = new LocalStrategy((username, password, done) => {
   const knex = dbGet();
   let user;
 
-  // knex.select('users.username', 'users.password')
-  //   .from('users')
-  //   .where('users.username', username)
-  //   .first()
-  //   .then(results => {
-  //     user = results;
-  //     if (!user) {
-  //       return Promise.reject({
-  //         reason: 'LoginError',
-  //         message: 'Incorrect username',
-  //         location: 'username'
-  //       });
-  //     }
-  //     return user.validatePassword(password);
-  //   })
-  //   .then(isValid => {
-  //     if (!isValid) {
-  //       return Promise.reject({
-  //         reason: 'LoginError',
-  //         message: 'Incorrect password',
-  //         location: 'password'
-  //       });
-  //     }
-  //     return done(null, user);
-  //   })
-  //   .catch(err => {
-  //     if(err.reason === 'LoginError') {
-  //       return done(null, false);
-  //     }
-  //     return done(err);
-  //   });
-
-  try {
-    if (username !== 'thinkfulstudent123') {
-      console.log('Incorrect username!');
-      return done(null, false);
-    }
-
-    if (password !== 'thinkful2018') {
-      console.log('Incorrect password!');
-      return done(null, false);
-    }
-
-    const user = { username, password };
-    return done(null, user);
-
-  } catch (err) {
-    done(err);
+  function validatePassword (password) {
+    return bcrypt.compare(password, 10);
   }
+
+  knex.select('users.username', 'users.password')
+    .from('users')
+    .where('users.username', username)
+    .first()
+    .then(results => {
+      user = results;
+      console.log(user);
+      if (!user) {
+        return Promise.reject({
+          reason: 'LoginError',
+          message: 'Incorrect username',
+          location: 'username'
+        });
+      }
+      // return validatePassword(user.password);
+    })
+    // .then(isValid => {
+    //   if (!isValid) {
+    //     return Promise.reject({
+    //       reason: 'LoginError',
+    //       message: 'Incorrect password',
+    //       location: 'password'
+    //     });
+    //   }
+    //   return done(null, user);
+    // })
+    .catch(err => {
+      if(err.reason === 'LoginError') {
+        return done(null, false);
+      }
+      return done(err);
+    });
+
+  // try {
+  //   if (username !== 'thinkfulstudent123') {
+  //     console.log('Incorrect username!');
+  //     return done(null, false);
+  //   }
+
+  //   if (password !== 'thinkful2018') {
+  //     console.log('Incorrect password!');
+  //     return done(null, false);
+  //   }
+
+  //   const user = { username, password };
+  //   return done(null, user);
+
+  // } catch (err) {
+  //   done(err);
+  // }
 
 });
 
