49a617126f221447b47f38fd3819e31298159f2b
Adrian <adriantoddross@gmail.com>
2018-05-02 20:20:52 -0400

    change fullname to firstname

--- db/manual-seed.sql
+++ db/manual-seed.sql
@@ -1,7 +1,7 @@
 INSERT into categories (title) VALUES
 ('Housing'), ('Childcare'), ('Food'), ('Healthcare'), ('Utilities'),('Auto'), ('Auto-pay'), ('Entertainment');
 
-INSERT into users (id, fullname, username, password) VALUES
+INSERT into users (id, firstname, username, password) VALUES
 (1, 'dev user', 'dev', '0320');
 
 INSERT into bills (category_id, user_id, name, amount) VALUES
