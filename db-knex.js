cee5e4d09968108e086a3818b529a7c2fa4b5004
Adrian <adriantoddross@gmail.com>
2018-05-18 16:36:22 -0400

    force pg to use SSL

--- db-knex.js
+++ db-knex.js
@@ -1,17 +1,17 @@
 'use strict';
 
 const createKnex = require('knex');
-
 const {DATABASE_URL} = require('./config');
-
+var pg = require('pg');
+pg.defaults.ssl = true; // enable SSL for Heroku Postgres
 let knex = null;
+
 
 function dbConnect(url = DATABASE_URL) {
   console.log('Connecting...');
   knex = createKnex({
     client: 'pg',
     connection: url
-
   });
 }
 
